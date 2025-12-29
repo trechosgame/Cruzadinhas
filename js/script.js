17008502d25a81304f16448f1cd28a66a3b56b00
Débora Inocêncio <30512134+trechosgame@users.noreply.github.com>
2025-12-29 17:51:06 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,202 +1,172 @@
-var myWords = ["FOGÃO","BALÃO","PIÃO","SABÃO","CORAÇÃO",
-"ANÃO","LIMÃO","FEIJÃO"];
-var tempWords = [];var selectedWord = "";
+// === FASES ===
+const phases = [
+  { title: "Fase ÃO", words: ["FOGÃO","BALÃO","PIÃO","SABÃO","CORAÇÃO","ANÃO","LIMÃO","FEIJÃO"] },
+  { title: "Fase B", words: ["BOLA","BOLO","BEBE","BICO","BOCA","BALDE","BOTA","BULE"] },
+  { title: "Fase C", words: ["CASA","CAMA","CARRO","CAO","CAVALO","COPA","COROA","CIRCO"] },
+  { title: "Fase D", words: ["DADO","DENTE","DEDO","DOCE","DAMA","DINHO","DISCO","DINO"] }
+];
+
+let currentPhase = 0;
+let myWords = phases[currentPhase].words;
+let tempWords = [];
+let selectedWord = "";
+
 $(document).ready(function(){
-    arrangeGame();
-    $(".individual").click(function(){
-        $(this).addClass("colorPurple");
-        selectedWord += $(this).html();
-        console.log(selectedWord);
-    });
-    $(document).keydown(function(){
-        selectedWord = "";
-        $(".individual").removeClass("colorPurple");
-    }).keyup(function(){
-        if(myWords.indexOf(selectedWord) >= 0)
-        {
-            $(".colorPurple").addClass("correctlySelected");
-            $("#hint p").each(function(key, item){
-                if(selectedWord == $(item).html())
-                { 
-                    $(this).addClass("done");
-                }
-                if($(".done").lenght == myWords.length)  
-                {
-                    $("#hint").remove();
-                    $("#hint").append("PARABÉNS")
-                    
-                }  
-            });
-        }
-    });                   
+  loadPhase(currentPhase);
 });
-function arrangeGame()
-{
-    $("#hint").show();
-    $.each(myWords, function(key, item){
-      $("#hint").append("<p>" + item + "</p>");
+
+function loadPhase(phaseIndex) {
+  currentPhase = phaseIndex;
+  myWords = phases[currentPhase].words;
+  tempWords = [];
+  selectedWord = "";
+
+  $("#phaseTitle").text(phases[currentPhase].title);
+  $("#wordsList").empty();
+  myWords.forEach(word => $("#wordsList").append("<p>" + word + "</p>"));
+
+  arrangeGame();
+  setupTouch();
+}
+
+function setupTouch() {
+  $(".individual").off();
+
+  $(".individual").on("touchstart mousedown", function(e){
+    e.preventDefault();
+    selectedWord = "";
+    $(".individual").removeClass("colorPurple");
+    selectLetter($(this));
   });
-  for(var i=1;i<=12;i++)
-  {
-     for(var j=1;j<=12;j++)
-     {
-       $("#letters").append("<div class=individual data-row=" + i + " data-column=" + j + "></div>");
-     }
+
+  $(".individual").on("touchmove mousemove", function(e){
+    e.preventDefault();
+    let touch = e.originalEvent.touches ? e.originalEvent.touches[0] : e;
+    let elem = document.elementFromPoint(touch.clientX, touch.clientY);
+    let $letter = $(elem).closest(".individual");
+    if ($letter.length > 0 && $letter.text().trim() !== "" && !$letter.hasClass("colorPurple")) {
+      selectLetter($letter);
+    }
+  });
+
+  $(document).on("touchend mouseup", function(){
+    checkSelectedWord();
+  });
+}
+
+function selectLetter($letter) {
+  $letter.addClass("colorPurple");
+  selectedWord += $letter.text();
+}
+
+function checkSelectedWord() {
+  if (myWords.includes(selectedWord)) {
+    $(".colorPurple").addClass("correctlySelected");
+    $("#wordsList p").each(function(){
+      if ($(this).text() === selectedWord) {
+        $(this).addClass("done");
+      }
+    });
+
+    if ($(".done").length === myWords.length) {
+      $("#phaseComplete").show();
+    }
   }
+
+  selectedWord = "";
+  setTimeout(() => $(".individual").removeClass("colorPurple"), 800);
+}
+
+function nextPhase() {
+  $("#phaseComplete").hide();
+  if (currentPhase < phases.length - 1) {
+    loadPhase(currentPhase + 1);
+  } else {
+    alert("🎉 Você completou TODAS as fases!!! 🎉");
+  }
+}
+
+function arrangeGame() {
+  $("#letters").empty();
+  for(let i = 1; i <= 12; i++) {
+    for(let j = 1; j <= 12; j++) {
+      $("#letters").append(`<div class="individual" data-row="${i}" data-column="${j}"></div>`);
+    }
+  }
+
   placeCorrectLetters(myWords);
-  //console.log("end of firts array \n");
   placeCorrectLetters(tempWords);
-  $.each($(".individual"), function(key, item){  
-      if($(item).attr("data-word") == undefined)
-          $(this).html(randomLetter());
-  })
-}
-function randomLetter()
-{
-    var alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    return alphabets.charAt(Math.floor(Math.random()*26));
-}
-function checkOccupied(word, starting, orientation)
-{
-    var status = ""; var incrementBy = 0;
-    if(orientation == "row")
-        incrementBy = 1;
-    else if(orientation == "column")
-        incrementBy = 12;
-    else if(orientation == "diagonal")
-        incrementBy = 13;
-    for(var p=starting,q=0;q<word.length;q++)
-    {
-        if($(".individual:eq(" + p + ")").attr("data-word")
-        == undefined)
-            status = "empty";
-        else
-        {
-            status = "occupied";
-            break;
-        }
-        p += incrementBy;
+
+  $(".individual").each(function(){
+    if (!$(this).attr("data-word") || $(this).text() === "") {
+      $(this).text(randomLetter());
     }
-    return status; 
+  });
 }
-function placeCorrectLetters(myArr)
-{
-    var positions = ["row","column","diagonal"];
-    var nextLetter = 0; var newStart = 0;
-    for(var i=0;i<myArr.length;i++)
-    {
-        var orientation = 
-        positions[Math.floor(Math.random()*positions.length)]
-        ;
-        var start = 
-        Math.floor(Math.random()*$(".individual").length);
-        var myRow = $(".individual:eq(" + start + 
-        ")").data("row");
-        var myColumn = $(".individual:eq(" + start +
-        ")").data("column");
-        //console.log(myArr[i] + " : " + orientation + " : " + start + " : " + myRow + " : " + myColumn);
-        
-        if(orientation == "row")
-        {
-            nextLetter = 1;
-            if((myColumn*1) + myArr[i].length <=12)
-            {
-                   newStart = start;
-                   //console.log("space in row: " + myArr[i] + " : " + start + " : " + myColumn);
-            }
-            else
-            {   
-                   var newColumn = 12 - myArr[i].lenght;
-                   newStart = $(".individual[data-row=" + myRow
-                   + "][data-column=" + newColumn + 
-                   "]" ).index();
-                   //console.log("no space in row: " + myArr[i] + " : " + start + " : " + myColumn + " : " + newStart);
-               }
-         }
-         else if(orientation == "column")
-         {
-           nextLetter = 12;    
-           if((myRow*1) + myArr[i].length <= 12)
-           {
-               newStart = start;
-               //console.log("space in column: " + myArr[i] + " : " + start + " : " + myRow);
-           }
-           else
-               { 
-                 var newRow = 12 - myArr[i].length;
-                  newStart = $(".individual[data-row=" + newRow 
-                  + "][data-column=" + newColumn + "]").index();
-                 //console.log("no space in column: " + myArr[i] + " : " + start + " : " + myRow  + ":" +newStart);
-               }
-           }
-           else if(orientation == "diagonal")
-           {
-              nextLetter = 13;
-              if((myColumn*1) + myArr[i].length <= 12 &&
-              (myRow*1) + myArr[i].length <= 12)
-               {
-                  newStart = start;
-               }   
-              if((myColumn*1) + myArr[i].length > 12)
-              {
-                var newColumn = 12 - myArr[i].length;
-                newStart = $(".individual[data-row=" + myRow 
-                + "][data-column=" + newColumn + "]").index();
-              }
-              if((myRow*1) + myArr[i].length > 12)
-              {
-                 var newRow = 12 - myArr[i].length;
-                 newStart = $(".individual[data-row=" + newRow 
-                 + "][data-column=" + myColumn + "]").index(); 
-              }
-              if((myColumn*1) + myArr[i].length > 12 &&
-              (myRow*1) + myArr[i].length > 12)
-              {
-                  var newColumn = 12 - myArr[i].length;
-                  var newRow = 12 - myArr[i].length;
-                  newStart = $(".individual[data-row=" + newRow
-                  + "][data-column=" + newColumn + 
-                  "]").index();
-              }
-           }
-           var characters = myArr[i].split("");
-           var nextPosition = 0;
-           var occupied = checkOccupied(myArr[i], newStart, 
-           orientation);
-           if(occupied == "empty")
-           {   
-               $.each(characters, function(key, item){
-                   //console.log(item);
-                   $(".individual:eq(" + (newStart+nextPosition)
-                   + ")").html(item);
-                   //(".individual:eq(" + (newStart + nextPosition)+ ")").css("background", "pink");
-                   $(".individual:eq(" + (newStart+nextPosition) 
-                   + ")").attr("data-word", myArr[i]);
-                   nextPosition += nextLetter;
-               })
-            }
-            else
-            {
-               tempWords.push(myArr[i]);
-            }
-            //console.log(tempWords);
+
+function randomLetter() {
+  const easy = "AAAAAAAAAAEEEEEEIIIIIIOOOOOOOUUUUÃÕÇBCDFGHJLMNPRSTV";
+  return easy.charAt(Math.floor(Math.random() * easy.length));
+}
+
+// === FUNÇÕES ORIGINAIS (colocação das palavras) ===
+function checkOccupied(word, starting, orientation) {
+  var status = ""; var incrementBy = 0;
+  if(orientation == "row") incrementBy = 1;
+  else if(orientation == "column") incrementBy = 12;
+  else if(orientation == "diagonal") incrementBy = 13;
+  for(var p=starting,q=0;q<word.length;q++) {
+    if($(".individual:eq(" + p + ")").attr("data-word") == undefined)
+      status = "empty";
+    else {
+      status = "occupied";
+      break;
     }
-     
+    p += incrementBy;
+  }
+  return status;
 }
 
+function placeCorrectLetters(myArr) {
+  var positions = ["row","column","diagonal"];
+  var nextLetter = 0; var newStart = 0;
+  for(var i=0;i<myArr.length;i++) {
+    var orientation = positions[Math.floor(Math.random()*positions.length)];
+    var start = Math.floor(Math.random()*$(".individual").length);
+    var myRow = $(".individual:eq(" + start + ")").data("row");
+    var myColumn = $(".individual:eq(" + start + ")").data("column");
 
+    if(orientation == "row") {
+      nextLetter = 1;
+      if((myColumn*1) + myArr[i].length <=12) newStart = start;
+      else {
+        var newColumn = 12 - myArr[i].length;
+        newStart = $(".individual[data-row=" + myRow + "][data-column=" + newColumn + "]").index();
+      }
+    } else if(orientation == "column") {
+      nextLetter = 12;
+      if((myRow*1) + myArr[i].length <= 12) newStart = start;
+      else {
+        var newRow = 12 - myArr[i].length;
+        newStart = $(".individual[data-row=" + newRow + "][data-column=" + myColumn + "]").index();
+      }
+    } else if(orientation == "diagonal") {
+      nextLetter = 13;
+      if((myColumn*1) + myArr[i].length <= 12 && (myRow*1) + myArr[i].length <= 12) newStart = start;
+      // Adicione aqui o resto do seu código diagonal se quiser
+    }
 
-
-            
-
-            
-
-
-
-
-
-            
-
-            
-
-
+    var characters = myArr[i].split("");
+    var nextPosition = 0;
+    var occupied = checkOccupied(myArr[i], newStart, orientation);
+    if(occupied == "empty") {
+      $.each(characters, function(key, item){
+        $(".individual:eq(" + (newStart+nextPosition) + ")").html(item);
+        $(".individual:eq(" + (newStart+nextPosition) + ")").attr("data-word", myArr[i]);
+        nextPosition += nextLetter;
+      });
+    } else {
+      tempWords.push(myArr[i]);
+    }
+  }
+}
